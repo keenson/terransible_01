fc606e9afa795ad8fcac1c61a2138bdf894b0989
keenson <keensono@gmail.com>
2021-03-12 04:35:57 +0100

    Devops_01

---
+++ terraform/web/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "name" {
+  description = "Name the instance on deploy"
+}
+resource "aws_instance" "devops_01_web" {
+  ami = "ami-013f17f36f8b1fefb"
+  instance_type = "t2.micro"
+  key_name = "devops_key"
+
+  tags = {
+    Name = var.name
+  }
+}
